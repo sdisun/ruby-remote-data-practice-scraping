ec5fd40b75ded6928ba27af9510fac4093b30bab
Neil <zooming.in@hotmail.com>
2021-08-23 20:49:29 +1000

    updates

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html) 
+heading_text = doc.css('.headline-26OIBN').text
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+puts "\n----HOMEPAGE HEADING----\n #{heading_text}"
+puts "\n----AVAILABLE COURSES----"
+courses.each {|course| puts course.text}
